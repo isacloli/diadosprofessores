f0dc12bfc69207d740cdb2c90e562854622c696e
isacloli <135846785+isacloli@users.noreply.github.com>
2023-10-11 11:00:04 -0300

    .

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var mostrarMensagemBtn = document.getElementById("mostrarMensagem");
 
     mostrarMensagemBtn.addEventListener("click", function () {
-        mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês, " +
+        mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês! " +
         "Nós alunos, agradecemos todos os esforços de vocês para nos ensinar as matérias e lições da vida."+
         "Que Deus os abençoe e dê forças para lutarem, tentar e fazer a diferença no mundo.";
     });
